ee436c9e630052b281a5c4b24c2d8dcf02f5174f
GugaSantos <guga.santos.guga@gmail.com>
2022-10-28 16:19:05 -0300

    feat: ensure its possible to generate a report through a csv file

---
+++ inventory_report/inventory/inventory.py
@@ -0,0 +1,20 @@
+import csv
+
+from ..reports.complete_report import CompleteReport
+from ..reports.simple_report import SimpleReport
+
+
+class Inventory:
+    @classmethod
+    def import_data(cls, path, type):
+        if type == "simples":
+            data = cls.read_data(path)
+            return SimpleReport.generate(data)
+        if type == "completo":
+            data = cls.read_data(path)
+            return CompleteReport.generate(data)
+
+    def read_data(list):
+        if list.endswith(".csv"):
+            with open(list, encoding="utf-8") as data:
+                return [*csv.DictReader(data)]
